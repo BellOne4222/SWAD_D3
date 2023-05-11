ee3706c265cd1f7474fc6582655c68d18bc08e8f
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-11 16:46:31 +0900

    5/11

--- 1208.py
+++ 1208.py
@@ -1,1 +1,14 @@
-# 1208 Flatten
+# 1208 Flatten  
+
+T = int(input())
+
+for test_case in range(10):
+    boxes = list(map(int, input().split()))
+    boxes.sort()
+    for i in range(T):
+        max[boxes] -= 1
+        min[boxes] += 1
+        if (max[boxes] - min[boxes]) <= 1:
+            ans = max[boxes] - min[boxes]
+            print("#{} {}".format(test_case, ans))
+    print("#{} {}".format(test_case, (max[boxes] - min[boxes])))
