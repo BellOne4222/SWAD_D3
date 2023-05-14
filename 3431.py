1fdcd6be79fde379d42638df4fd166855c1afd13
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-14 18:22:39 +0900

    5/14

--- 3431.py
+++ 3431.py
@@ -22,10 +22,12 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     L, U, X = map(int, input().split())
-    if U < X:
-        ans = -1
-    elif X < L:
-        ans = L - X
-    else:
+    
+    if L <= X <= U: # 범위 내이면 0반환
         ans = 0
+    elif X > U: # 최대 시간 보다 크면 -1 반환
+        ans = -1
+    elif L > X: # 최소 시간 보다 작으면 최소 시간 - 이번주에 운동한 시간 반환
+        ans = L - X
+        
     print("#{} {}".format(test_case, ans))
