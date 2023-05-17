18e494dc3c8dd69b49b7dee9d553a5bd3d37ec80
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-17 20:29:10 +0900

    5/17

--- 5789.py
+++ 5789.py
@@ -30,12 +30,11 @@ T = int(input())
 for test_case in range(1, T+1):
     N, Q = map(int, input().split())
     zero_arr = [0 for _ in range(N)]
-    for i in range(1, Q+1):
+    for i in range(Q):
         L, R = map(int, input().split())
         for j in range(L-1, R):
-            zero_arr[j] = i
+            zero_arr[j] = i+1
     
     ans = ' '.join(map(str, zero_arr))
 
     print("#{} {}".format(test_case, ans))
-    
