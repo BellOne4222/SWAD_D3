b9b6ad783d9141a74047eeb3c502b868f314823d
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-05-17 20:14:27 +0900

    5/17

--- practice.py
+++ practice.py
@@ -1,9 +1,11 @@
-cnt = 0
-n = 0
-count = 0
-while cnt == 10000:
+n = 1
+cnt = 1
+
+while True:
     n += 1
     cnt += n
-    count += 1
+    print(n)
+    if cnt == 10000:
+        break
 
-print(count)
+print(n)
